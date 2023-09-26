67694fb100225619373f2b1f3756d2d9042ab50b
ivan-aim <77643196+ivan-aim@users.noreply.github.com>
2023-09-26 11:29:41 -0400

    Enhance Amazon banner display

--- components/Amazon/Banner.vue
+++ components/Amazon/Banner.vue
@@ -32,38 +32,68 @@
 
 
   // === Data ===
-  const state = reactive({
-    height: props.height,
-    width: props.width,
-  })
+  const PADDING_AD = 5;
+  interface StateObject {
+    disclaimerText: string;
+    widthWindow: number | undefined;
+  }
+  const DEFAULT_DISCLAIMER_TEXT = 'Ad • Amazon';
+  const state: StateObject = reactive({
+    disclaimerText: DEFAULT_DISCLAIMER_TEXT,
+    widthWindow: undefined,
+  });
+
+
+  // === Computed Fields ===
+  /**
+   * Adjust image dimension to fit into parent window
+   */
+  const imgDimension = computed(() => {
+    const widthRequiredForImage = (props.width + PADDING_AD * 2);
+    if (state.widthWindow && state.widthWindow < widthRequiredForImage) {
+      const newWidthImage = state.widthWindow - (PADDING_AD * 2);
+      return {
+        height: props.height * (newWidthImage / props.width),
+        width: newWidthImage,
+      }
+    }
+
+    return {
+      height: props.height,
+      width: props.width,
+    }
+  });
+
+
+  // === Methods ===
+  const swapDisclaimerText = () => {
+    state.disclaimerText = state.disclaimerText === DEFAULT_DISCLAIMER_TEXT ?
+      'Disclaimer: As an Amazon Associate I earn from qualifying purchases' :
+      DEFAULT_DISCLAIMER_TEXT;
+  };
+
 
   // === Life Cycle Hooks ===
   onMounted(() => {
     /**
-     * Resize banner to fit inside parent window
+     * Record new window width to support resize of banner to fit inside parent window
      */
-    const resizeImageBasedOnWindowWidth = (event: Event) => {
-      const widthWindow = (event.target as Window)?.innerWidth;
-      // console.log(`[${utility.currentFileName}::resizeImageBasedOnWindowWidth] widthWindow:`, widthWindow);
-
-      if (widthWindow && widthWindow < props.width) {
-        // console.log(`[${utility.currentFileName}::resizeImageBasedOnWindowWidth] widthWindow, props.width:`, widthWindow, props.width);
-        state.width = widthWindow;
-        state.height = props.height * (state.width / props.width);
-        return;
-      }
-
-      state.height = props.height;
-      state.width = props.width;
+    const recordWindowWidth = (event: Event) => {
+      state.widthWindow = (event.target as Window)?.innerWidth;
+      // console.log(`[${utility.currentFileName}::recordWindowWidth] state.widthWindow:`, state.widthWindow);
     };
 
-    window.onload = resizeImageBasedOnWindowWidth;
-    window.onresize = resizeImageBasedOnWindowWidth;
+    window.onload = recordWindowWidth;
+    window.onresize = recordWindowWidth;
   });
 </script>
 
 <template>
-  <div class="image-link">
+  <div class="ad-wrapper">
+    <div class="disclaimer" @click="swapDisclaimerText">
+      {{ state.disclaimerText }}
+    </div>
+
     <NuxtLink
       rel="nofollow noopener"
       target="_blank"
@@ -71,32 +101,35 @@
     >
       <img
         :alt="imageAltText"
-        :height="state.height"
+        :height="imgDimension.height"
         :src="image"
-        :width="state.width"
+        :width="imgDimension.width"
       />
     </NuxtLink>
-  </div>
-
-  <div class="disclaimer">
-    Disclaimer: As an Amazon Associate I earn from qualifying purchases
   </div>
 </template>
 
 <style scoped>
-.image-link {
-  /* 0 line-height removes gap between image and disclaimer */
-  line-height: 0px;
+.ad-wrapper {
+  background-color: white;
+  border-radius: calc(v-bind(PADDING_AD) * 1px);
+  margin-left: auto;
+  margin-right: auto;
+  width: calc(v-bind(imgDimension.width) * 1px + v-bind(PADDING_AD) * 2px);
 }
 
 .disclaimer {
-  background-color: white;
-  color: black;
+  background: linear-gradient(to bottom left, black, #999);
+  border-radius: calc(v-bind(PADDING_AD) * 1px) calc(v-bind(PADDING_AD) * 1px) 0px 0px;
+  color: white;
+  cursor: pointer;
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 9px;
+  font-size: 0.75rem;
   margin-left: auto;
   margin-right: auto;
   margin-top: 1px;
-  width: calc(v-bind(state.width) * 1px);
+  margin-bottom: 5px;
+  padding: calc(v-bind(PADDING_AD) * 1px);
+  width: calc(v-bind(imgDimension.width) * 1px);
 }
 </style>
